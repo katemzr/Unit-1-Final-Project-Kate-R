0c52652d245fb45627672b48168c815a642145ea
katemzr <katemzr@gmail.com>
2026-07-12 17:41:07 -0500

    Added an unordered list to the About page.

--- web-app-project/src/components/pages/AboutPage.jsx
+++ web-app-project/src/components/pages/AboutPage.jsx
@@ -1,8 +1,21 @@
 const AboutPage = () => {
     return (
         <div>
-            <h1>About "The Last Piece" goes here.</h1>
-        </div>
+            <h1>About 'The Last Piece'</h1>
+            <p>'The Last Piece' web app is my LaunchCode Unit 1 Final Project. The product must include the following:</p>
+
+            <ul>
+                <li>Home page with an introduction.</li>
+                <li>About page that is separate from the home page.</li>
+                <li>Easy-to-follow flow between pages for users, linked using React Router.</li>
+                <li>Header component with a title.</li>
+                <li>Footer with text or links to additional resources.</li>
+                <li>Features where users can add, modify, OR delete content.</li>
+                <li>At least one interactive feature.</li>
+            </ul>
+
+            
+        </div> 
     );
 };
 
