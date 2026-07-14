ed3b8b26327136d2056468d2f2a1eef2df9136a2
katemzr <katemzr@gmail.com>
2026-07-14 16:27:04 -0500

    changed the placeholder for login credentials. Added credentials to labels instead.

--- web-app-project/src/components/LoginForm.jsx
+++ web-app-project/src/components/LoginForm.jsx
@@ -17,11 +17,11 @@ const LoginForm = ({onLogin}) => {
             <form onSubmit={handleSubmit}>
               <h1>Hello, Puzzler! Please log in.</h1>
 
-                <label htmlFor="username">Username:</label>
-                <input type="text" id="username" name="username" placeholder="Enter: admin" value={username} required onChange={(e) => setUsername(e.target.value)}/>
+                <label htmlFor="username">Username: admin</label>
+                <input type="text" id="username" name="username" placeholder="Enter the username." value={username} required onChange={(e) => setUsername(e.target.value)}/>
 
                 <label htmlFor="password">Password: 1234</label>
-                <input type="password" id="password" name="password" placeholder="Enter: 1234" value={password} required onChange={(e) => setPassword(e.target.value)}/>
+                <input type="password" id="password" name="password" placeholder="Enter the password." value={password} required onChange={(e) => setPassword(e.target.value)}/>
                 
 
                 <button className="buttons" type="submit">Login</button> 
