a3529dcdd19f989b8d43743e9521c22235acf102
katemzr <katemzr@gmail.com>
2026-07-15 16:27:39 -0500

    Commented out media query code. Looking for other options.  Also, fixed syntax bug by adding <table> to wrap the table on About page.

--- web-app-project/src/components/pages/AboutPage.jsx
+++ web-app-project/src/components/pages/AboutPage.jsx
@@ -3,11 +3,10 @@ import Animation from "../Animation";
 
 const AboutPage = () => {
     return (
-        <div>
-            <h1>About 'The Last Piece'</h1>
-            
+        <>
+            <h1>About 'The Last Piece'</h1>    
             <p><b>'The Last Piece' web app is my LaunchCode Unit 1 Final Project. The product must include the following:</b></p>
-
+        
             <ul className="list">
                 <li>Home page with an introduction.</li>
                 <li>About page that is separate from the home page.</li>
@@ -18,39 +17,39 @@ const AboutPage = () => {
                 <li>At least one interactive feature.</li>
                 <li>Simple animation(s) - perhaps an orange square that spins around.</li>
             </ul>
-
-            <thead className="table">
-                <tr>
-                    <th>
-                        <b>My Favorite Puzzles</b>
-                    </th>
-                    <th>
-                        Why I Like This Puzzle
-                    </th>
-                </tr>
-                <tr>
-                    <td>
-                        Life
-                    </td>
-                    <td>
-                        The reason I love this puzzle is because at the time I bought it, it was the world's largest puzzle - measuring 14 feet by 5 feet.
-                    </td>
-                </tr>
-                <tr>
-                    <td>Farmers Market</td>
-                    <td>This one is just cute - cute as heck!</td>
-                </tr>
-                <tr>
-                    <td>Turbo 3000</td>
-                    <td>This one was a gift - as part of a set - for Christmas, from my boyfriend.</td>
-                </tr>
-            </thead>
-
+            <table>
+                <thead className="table">
+                    <tr>
+                        <th>
+                            <b>My Favorite Puzzles</b>
+                        </th>
+                        <th>
+                            Why I Like This Puzzle
+                        </th>
+                    </tr>
+                    <tr>
+                        <td>
+                            Life
+                        </td>
+                        <td>
+                            The reason I love this puzzle is because at the time I bought it, it was the world's largest puzzle - measuring 14 feet by 5 feet.
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Farmers Market</td>
+                        <td>This one is just cute - cute as heck!</td>
+                    </tr>
+                    <tr>
+                        <td>Turbo 3000</td>
+                        <td>This one was a gift - as part of a set - for Christmas, from my boyfriend.</td>
+                    </tr>
+                </thead>
+            </table>
             <div>
                 <Animation />
             </div>
 
-        </div> 
+        </>
     );
 };
 
